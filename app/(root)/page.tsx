7f2481799a29b7302d9900d5e691fee3dc19e8bc
s0560996 <ododmengapa@gmail.com>
2025-05-21 06:52:45 +0200

    First Commit, frontend structure

---
+++ app/(root)/page.tsx
@@ -0,0 +1,14 @@
+import sampleData from "@/db/sample-data";
+import ProductList from "@/components/shared/product/product-list";
+export const metadata = {
+  title: "Home",
+};
+const Homepage = () => {
+  return <>
+    <ProductList data={sampleData.products} title='Newest Arrivals' limit={4}>
+
+    </ProductList>
+  </>;
+};
+
+export default Homepage;
